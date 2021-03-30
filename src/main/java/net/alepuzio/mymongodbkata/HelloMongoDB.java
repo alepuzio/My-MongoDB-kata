822d9a96673d0f27c78cec3320af0c97c982c9fa
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-30 10:29:58 +0200

    added src dir

---
+++ src/main/java/net/alepuzio/mymongodbkata/HelloMongoDB.java
@@ -0,0 +1,9 @@
+package net.alepuzio.mymongodbkata;
+/**
+ * from https://developer.mongodb.com/quickstart/java-setup-crud-operations/
+ * */
+public class HelloMongoDB {
+    public static void main(String[] args) {
+        System.out.println("Hello MongoDB!");
+    }
+}
